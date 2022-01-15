aad89784b21accdc815498b0e934c760befbf6e2
ErranderLee <dre12am14@gmail.com>
2022-01-16 03:45:33 +0900

    JPA 기초

--- src/main/java/hellojpa/Jpamain.java
+++ src/main/java/hellojpa/Jpamain.java
@@ -2,14 +2,28 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class Jpamain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
